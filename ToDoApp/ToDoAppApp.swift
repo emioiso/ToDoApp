a997433b6eaa20087331083c04048ea145dff02a
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-03 23:59:33 +0900

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by emi oiso on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
