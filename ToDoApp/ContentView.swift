a1946b23617e313242d66dcf06fedbe452309737
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-04 16:04:52 +0900

    .gitignoreとToDoApp作れた！

--- ToDoApp/ContentView.swift
+++ ToDoApp/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskData = [
+        (title:"ジョギングする",completed:false),
+        (title:"お花にお水をやる",completed:false),
+        (title:"部屋の掃除をする",completed:false),
+        (title:"本を読む",completed:false),
+        (title:"トイレ掃除をする",completed:false)
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(0..<taskData.count, id: \.self) {index in
+                Button {
+//                    taskData[index].completed = !taskData[index].completed
+//                    or ↕︎
+                    taskData[index].completed.toggle()
+                } label: {
+                    HStack{
+//                        if(taskData[index].completed == true){
+//                            Image(systemName: "checkmark.circle.fill")
+//                        } else {
+//                            Image(systemName: "circle")
+//                        }
+                        // or ↕︎
+                        Image(systemName: taskData[index].completed == true ? "checkmark.circle.fill": "circle")
+                        Text(taskData[index].title);
+                    }
+                }
+                .foregroundColor(.primary)
+            }
+            .navigationTitle("ToDoリスト")
         }
-        .padding()
     }
 }
 
